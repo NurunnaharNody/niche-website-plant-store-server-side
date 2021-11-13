c83cf9838ae3079ff95daa8ee68a0badd0213a42
Nurunnahar Nody <nurunnaharnody@gmail.com>
2021-11-13 10:36:12 +0600

    updated

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ async function run(){
         app.get('/orders/:email', async(req, res) =>{
             const email = req.params.email;
             const query = {email: email};
-            const order = await ordersCollection.find(query);
+            const order = await ordersCollection.filter(query);
             res.json(order);
         })
 
